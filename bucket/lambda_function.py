d2ff0045e65ea6cce08d1937be2dab670eb3c99e
Azra Eiden <carter.eiden@gmail.com>
2023-01-28 21:22:54 -0500

    Removed Notification stuff from Bucket

--- bucket/lambda_function.py
+++ bucket/lambda_function.py
@@ -47,8 +47,6 @@ def lambda_handler(event, context):
         remove_all_tags(bucket_name)
         put_bucket_website(bucket_name, cdef)
         delete_bucket_website(bucket_name)
-        put_bucket_notification_configuration(bucket_name, cdef)
-        delete_bucket_notification_configuration(bucket_name)
         delete_bucket(bucket_name)
 
         return eh.finish()
@@ -137,11 +135,6 @@ def get_bucket(bucket_name, cdef, region, prev_state):
         else:
             eh.add_op("delete_bucket_website")
 
-        if cdef.get("notification_configuration"):
-            eh.add_op("put_bucket_notification_configuration")
-        else:
-            eh.add_op("delete_bucket_notification_configuration")
-
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "NoSuchBucket":
             eh.add_log("Bucket Does Not Exist", {"bucket_name": bucket_name})
@@ -190,9 +183,6 @@ def create_bucket(bucket_name, cdef, region):
 
         if cdef.get("website_configuration"):
             eh.add_op("put_bucket_website")
-
-        if cdef.get("notification_configuration"):
-            eh.add_op("put_bucket_notification_configuration")
 
         eh.add_props({
             "name": bucket_name,
@@ -527,124 +517,6 @@ def delete_bucket_website(bucket_name):
             eh.retry_error("Bucket Website Error", 90)
             eh.add_log("Bucket Website Error", {"error": str(e)}, is_error=True)
 
-
-@ext(handler=eh, op="put_bucket_notification_configuration")
-def put_bucket_notification_configuration(bucket_name, cdef):
-    s3 = boto3.client("s3")
-
-    notification_config = cdef.get("notification_configuration")
-    # Looks like the following:
-    # [
-    #    {
-    #        "arn": <either a function arn, a queue arn, or a topic arn>,
-    #        "events": [<list of events>],
-    #        "filter_prefixes": [<list of prefixes>],
-    #        "filter_suffixes": [<list of suffixes>]
-    #    }
-    # ]
-
-    print(f"notification_config = {notification_config}")
-
-    validate_notification_config(notification_config)
-
-    not_config = {}
-    for config in notification_config:
-        if config.get("arn").startswith("arn:aws:lambda"):
-            if not not_config.get("LambdaFunctionConfigurations"):
-                not_config["LambdaFunctionConfigurations"] = []
-            not_config["LambdaFunctionConfigurations"].append(remove_none_attributes({
-                "LambdaFunctionArn": config.get("arn"),
-                "Events": config.get("events"),
-                "Filter": remove_none_attributes({
-                    "Key": remove_none_attributes({
-                        "FilterRules": ([
-                            remove_none_attributes({
-                                "Name": "prefix",
-                                "Value": prefix
-                            }) for prefix in config.get("filter_prefixes", [])
-                        ] + [
-                            remove_none_attributes({
-                                "Name": "suffix",
-                                "Value": suffix
-                            }) for suffix in config.get("filter_suffixes", [])
-                        ]) or None
-                    }) or None
-                }) or None
-            }))
-        
-        elif config.get("arn").startswith("arn:aws:sqs"):
-            if not "QueueConfigurations" in not_config:
-                not_config["QueueConfigurations"] = []
-            not_config["QueueConfigurations"].append(remove_none_attributes({
-                "QueueArn": config.get("arn"),
-                "Events": config.get("events"),
-                "Filter": remove_none_attributes({
-                    "Key": remove_none_attributes({
-                        "FilterRules": ([
-                            remove_none_attributes({
-                                "Name": "prefix",
-                                "Value": prefix
-                            }) for prefix in config.get("filter_prefixes", [])
-                        ] + [
-                            remove_none_attributes({
-                                "Name": "suffix",
-                                "Value": suffix
-                            }) for suffix in config.get("filter_suffixes", [])
-                        ]) or None
-                    }) or None
-                }) or None
-            }))
-
-        elif config.get("arn").startswith("arn:aws:sns"):
-            if not not_config.get("TopicConfigurations"):
-                not_config["TopicConfigurations"] = []
-            not_config["TopicConfigurations"].append(remove_none_attributes({
-                "TopicArn": config.get("arn"),
-                "Events": config.get("events"),
-                "Filter": remove_none_attributes({
-                    "Key": remove_none_attributes({
-                        "FilterRules": ([
-                            remove_none_attributes({
-                                "Name": "prefix",
-                                "Value": prefix
-                            }) for prefix in config.get("filter_prefixes", [])
-                        ] + [
-                            remove_none_attributes({
-                                "Name": "suffix",
-                                "Value": suffix
-                            }) for suffix in config.get("filter_suffixes")
-                        ]) or None
-                    }) or None
-                }) or None
-            }))
-
-
-    params = {
-        "Bucket": bucket_name,
-        "NotificationConfiguration": not_config,
-        "SkipDestinationValidation": True
-    }
-
-    print(f"params = {params}")
-
-    try:
-        _ = s3.put_bucket_notification_configuration(**params)
-        eh.add_log("Notification Config Set", {"config": notification_config})
-
-    except ClientError as e:
-        handle_common_errors(e, eh, "Put Bucket Notification Config Error", 90)
-
-@ext(handler=eh, op="delete_bucket_notification_configuration")
-def delete_bucket_notification_configuration(bucket_name):
-    s3 = boto3.client("s3")
-
-    try:
-        _ = s3.put_bucket_notification_configuration(Bucket=bucket_name, NotificationConfiguration={})
-        eh.add_log("Notification Config Deleted", {"bucket_name": bucket_name})
-
-    except ClientError as e:
-        handle_common_errors(e, eh, "Delete Bucket Notification Config Error", 90)
-
 #Not implementing yet
 @ext(handler=eh, op="delete_bucket")
 def delete_bucket(bucket_name):
@@ -691,65 +563,5 @@ def generic_render_def(obj, update_f):
         return update_f(obj)
     else:
         return obj
-
-def validate_notification_config(config):
-    schema = {
-        "type": "array",
-        "items": {
-            "type": "object",
-            "properties": {
-                "arn": {
-                    "type": "string",
-                    "description": "The ARN of the lambda function, SNS topic, or SQS queue to send notifications to"
-                },
-                "events": {
-                    "type": "array",
-                    "description": "The events to send notifications for",
-                    "minItems": 1,
-                    "uniqueItems": True,
-                    "items": {
-                        "type": "string",
-                        "enum": [
-                            's3:ReducedRedundancyLostObject',
-                            's3:ObjectCreated:*','s3:ObjectCreated:Put',
-                            's3:ObjectCreated:Post','s3:ObjectCreated:Copy',
-                            's3:ObjectCreated:CompleteMultipartUpload',
-                            's3:ObjectRemoved:*','s3:ObjectRemoved:Delete',
-                            's3:ObjectRemoved:DeleteMarkerCreated',
-                            's3:ObjectRestore:*','s3:ObjectRestore:Post',
-                            's3:ObjectRestore:Completed','s3:Replication:*',
-                            's3:Replication:OperationFailedReplication',
-                            's3:Replication:OperationNotTracked',
-                            's3:Replication:OperationMissedThreshold',
-                            's3:Replication:OperationReplicatedAfterThreshold',
-                            's3:ObjectRestore:Delete','s3:LifecycleTransition',
-                            's3:IntelligentTiering','s3:ObjectAcl:Put',
-                            's3:LifecycleExpiration:*',
-                            's3:LifecycleExpiration:Delete',
-                            's3:LifecycleExpiration:DeleteMarkerCreated',
-                            's3:ObjectTagging:*',
-                            's3:ObjectTagging:Put','s3:ObjectTagging:Delete',
-                        ],
-                    }
-                },
-                "filter_prefixes": {
-                    "type": "array",
-                    "description": "The prefixes to filter notifications for",
-                    "items": {
-                        "type": "string"
-                    }
-                },
-                "filter_suffixes": {
-                    "type": "array",
-                    "description": "The suffixes to filter notifications for",
-                    "items": {
-                        "type": "string"
-                    }
-                }
-            },
-            "required": ["arn", "events"]
-        }
-    }
-    fastjsonschema.validate(schema, config)
     
 
