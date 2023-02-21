3c9e333b34f9aefa619f12a9219db86d7be7065d
Azra Eiden <carter.eiden@gmail.com>
2023-02-20 21:14:06 -0500

    Fix

--- bucket/lambda_function.py
+++ bucket/lambda_function.py
@@ -545,7 +545,7 @@ def delete_bucket():
     try:
         s3 = boto3.resource('s3')
         s3_bucket = s3.Bucket(bucket_name)
-        bucket_versioning = s3.BucketVersioning()
+        bucket_versioning = s3.BucketVersioning(bucket_name)
         if bucket_versioning.status == 'Enabled':
             s3_bucket.object_versions.delete()
             eh.add_log("Deleted All Object Versions", {"bucket_name": bucket_name})
