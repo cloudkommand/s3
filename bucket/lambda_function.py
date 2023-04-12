361a3b261a03d016bc6bf07dcb41b0d88712776a
Azra Eiden <carter.eiden@gmail.com>
2023-04-12 10:13:44 -0400

    Using Gets and Adding Encryption Configuration

--- bucket/lambda_function.py
+++ bucket/lambda_function.py
@@ -44,17 +44,22 @@ def lambda_handler(event, context):
 
         get_bucket(cdef, region, prev_state)
         create_bucket(cdef, region, allow_alternate_bucket_name)
+        get_public_access_block(cdef)
         set_public_access_block(cdef)
         delete_public_access_block()
+        get_bucket_policy(cdef)
         set_bucket_policy(cdef)
         delete_bucket_policy()
         set_versioning(cdef)
+        get_cors(cdef)
         set_cors(cdef)
         delete_cors()
         set_tags()
         remove_all_tags()
         put_bucket_website(cdef)
         delete_bucket_website()
+        get_bucket_encryption(cdef)
+        put_bucket_encryption()
         delete_bucket()
 
         return eh.finish()
@@ -125,21 +130,9 @@ def get_bucket(cdef, region, prev_state):
 
         eh.add_links({"Bucket": gen_bucket_link(bucket_name)})
         eh.add_op("set_bucket_acl")
-
-        if cdef.get("block_public_access") or cdef.get("public_access_block"):
-            eh.add_op("set_public_access_block")
-        else:
-            eh.add_op("delete_public_access_block")
-
-        if cdef.get("bucket_policy"):
-            eh.add_op("set_bucket_policy")
-        else:
-            eh.add_op("delete_bucket_policy")
-
-        if cdef.get("CORS"):
-            eh.add_op("set_cors")
-        else:
-            eh.add_op("delete_cors")
+        eh.add_op("get_public_access_block")
+        eh.add_op("get_bucket_policy")
+        eh.add_op("get_bucket_cors")
 
         if cdef.get("tags"):
             eh.add_op("set_tags", cdef['tags'])
@@ -150,6 +143,8 @@ def get_bucket(cdef, region, prev_state):
             eh.add_op("put_bucket_website")
         else:
             eh.add_op("delete_bucket_website")
+
+        eh.add_op("get_bucket_encryption")
 
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "NoSuchBucket":
@@ -169,7 +164,7 @@ def create_bucket(cdef, region, allow_alternate_bucket_name):
 
     print(f"region = {region}")
     params = remove_none_attributes({
-        "ACL": cdef.get("ACL"),
+        # "ACL": cdef.get("ACL"),
         "Bucket": bucket_name,
         "CreateBucketConfiguration": {
             "LocationConstraint": region
@@ -225,22 +220,58 @@ def create_bucket(cdef, region, allow_alternate_bucket_name):
             eh.retry_error("Create Error", 0)
             eh.add_log("Create Error", {"error": str(e)}, is_error=True)
 
+@ext(handler=eh, op="get_bucket_acl")
+def get_bucket_acl(cdef):
+    bucket_name = eh.state.get("bucket_name")
+    try:
+        response = s3.get_bucket_acl(Bucket=bucket_name)
+        eh.add_log("Got Bucket ACL", response)
+        current_grants = list(filter(lambda grant: grant.get("ID") != response['Owner']['ID'], response.get("Grants")))
+        desired_params = get_acl_params_from_cdef(cdef)
+        desired_grants = desired_params.get("AccessControlPolicy", {}).get("Grants", [])
+        if desired_params.get("GrantFullControl"):
+            desired_grants.append({
+                "Grantee": get_grantee_from_grant_string(desired_params.get("GrantFullControl")),
+                "Permission": "FULL_CONTROL"
+            })
+        if desired_params.get("GrantRead"):
+            desired_grants.append({
+                "Grantee": get_grantee_from_grant_string(desired_params.get("GrantRead")),
+                "Permission": "READ_ACP"
+            })
+        if desired_params.get("GrantReadACP"):
+            desired_grants.append({
+                "Grantee": get_grantee_from_grant_string(desired_params.get("GrantReadACP")),
+                "Permission": "READ_ACP"
+            })
+        if desired_params.get("GrantWrite"):
+            desired_grants.append({
+                "Grantee": get_grantee_from_grant_string(desired_params.get("GrantWrite")),
+                "Permission": "WRITE_ACP"
+            })
+        if desired_params.get("GrantWriteACP"):
+            desired_grants.append({
+                "Grantee": get_grantee_from_grant_string(desired_params.get("GrantWriteACP")),
+                "Permission": "WRITE_ACP"
+            })
+
+        for grant in desired_grants:
+            matching_grant = list(filter(lambda x: x["Grantee"].get("ID") == grant["Grantee"].get("ID") and x["Grantee"].get("URI") == grant["Grantee"].get("URI") and x["Permission"] == grant["Permission"], current_grants))
+            if len(matching_grant) == 0:
+                eh.add_op("set_bucket_acl")
+                break
+
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "NoSuchBucket":
+            eh.add_log("Bucket Does Not Exist", {"bucket_name": bucket_name})
+        else:
+            handle_common_errors(e, eh, "Get Bucket ACL Error", 5)
 
 @ext(handler=eh, op="set_bucket_acl")
 def set_bucket_acl(cdef):
     bucket_name = eh.state.get("bucket_name")
 
-    if cdef.get("public_read") == True:
-        params = {
-            "GrantRead": "uri=http://acs.amazonaws.com/groups/global/AllUsers"
-        }
-
-    elif cdef.get("acl"):
-        params = cdef.get("acl") or {}
-
-    else:
-        params = {"ACL": "private"}
-
+    params = get_acl_params_from_cdef(cdef)
     params['Bucket'] = bucket_name
     try:
         _ = s3.put_bucket_acl(**params)
@@ -260,20 +291,35 @@ def set_bucket_acl(cdef):
             eh.add_log("Put ACL Error", {"error": str(e)}, is_error=True)
 
 
+@ext(handler=eh, op="get_public_access_block")
+def get_public_access_block(cdef):
+    bucket_name = eh.state.get("bucket_name")
+
+    desired_public_access_block = get_public_access_block_from_cdef(cdef)
+
+    try:
+        response = s3.get_public_access_block(Bucket=bucket_name)
+        eh.add_log("Got Public Access Block", response)
+    except ClientError as e:
+        if e.response['Error']['Code'] == "NoSuchPublicAccessBlockConfiguration":
+            response = {}
+            eh.add_log("No Public Access Block", response)
+        else:
+            handle_common_errors(e, eh, "Get Public Access Block Error", 12)
+            return
+    
+    if response.get("PublicAccessBlockConfiguration") != desired_public_access_block:
+        if desired_public_access_block:
+            eh.add_op("set_public_access_block")
+        else:
+            eh.add_op("delete_public_access_block")
+
+
 @ext(handler=eh, op="set_public_access_block")
 def set_public_access_block(cdef):
     bucket_name = eh.state.get("bucket_name")
 
-    if (cdef.get("block_public_access") == True or cdef.get("public_access_block") == True):
-        public_access_block = {
-            "BlockPublicAcls": True,
-            "IgnorePublicAcls": True,
-            "BlockPublicPolicy": True,
-            "RestrictPublicBuckets": True
-        }
-    
-    else:
-        public_access_block = cdef.get("public_access_block")
+    public_access_block = get_public_access_block_from_cdef(cdef)
 
     try:
         _ = s3.put_public_access_block(Bucket=bucket_name, PublicAccessBlockConfiguration=public_access_block)
@@ -313,13 +359,36 @@ def delete_public_access_block():
             eh.retry_error("Public Access Block Error", 15)
             eh.add_log("Public Access Block Error", {"error": str(e)}, is_error=True)
 
+@ext(handler=eh, op="get_bucket_policy")
+def get_bucket_policy(cdef):
+    bucket_name = eh.state.get("bucket_name")
+
+    desired_bucket_policy = json.dumps(render_bucket_policy(cdef.get("bucket_policy") or {}, bucket_name), sort_keys=True)
+    print(f"Rendered Bucket Policy: {desired_bucket_policy}")
+    try:
+        response = s3.get_bucket_policy(Bucket=bucket_name)
+        eh.add_log("Got Bucket Policy", response)
+        bucket_policy = json.dumps(json.loads(response.get("Policy")), sort_keys=True)
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == "NoSuchBucketPolicy":
+            bucket_policy = ""
+            eh.add_log("No Bucket Policy", {"bucket_name": bucket_name})
+        else:
+            handle_common_errors(e, eh, "Get Bucket Policy Error", 20)
+            return
+
+    if bucket_policy != desired_bucket_policy:
+        if desired_bucket_policy:
+            eh.add_op("set_bucket_policy")
+        else:
+            eh.add_op("delete_bucket_policy")
 
 @ext(handler=eh, op="set_bucket_policy")
 def set_bucket_policy(cdef):
     bucket_name = eh.state.get("bucket_name")
 
-    bucket_policy = render_bucket_policy(cdef.get("bucket_policy"), bucket_name)
-    print(f"Rendered Bucket Policy: {bucket_policy}")
+    bucket_policy = render_bucket_policy(cdef.get("bucket_policy") or {}, bucket_name)
     try:
         response = s3.put_bucket_policy(Bucket=bucket_name, Policy=json.dumps(bucket_policy))
         eh.add_log("Put Bucket Policy", response)
@@ -384,23 +453,35 @@ def set_versioning(cdef):
             eh.retry_error("Bucket Versioning Error", 50)
             eh.add_log("Bucket Versioning Error", {"error": str(e)}, is_error=True)
 
+@ext(handler=eh, op="get_bucket_cors")
+def get_cors(cdef):
+    bucket_name = eh.state.get("bucket_name")
+
+    desired_cors_config = get_cors_config_from_cdef(cdef)
+
+    try:
+        cors_config = s3.get_bucket_cors(Bucket=bucket_name)
+        eh.add_log("Got Bucket CORS", cors_config)
+    except ClientError as e:
+        if e.response['Error']['Code'] == "NoSuchCORSConfiguration":
+            cors_config = {}
+            eh.add_log("No Bucket CORS", {"bucket_name": bucket_name})
+        else:
+            handle_common_errors(e, eh, "Get Bucket CORS Error", 60)
+            return
+
+    if cors_config != desired_cors_config:
+        if desired_cors_config:
+            eh.add_op("set_cors")
+        else:
+            eh.add_op("delete_cors")
+
+
 @ext(handler=eh, op="set_cors")
 def set_cors(cdef):
     bucket_name = eh.state.get("bucket_name")
 
-    if cdef.get("CORS") == True:
-        cors_config = {"CORSRules": [
-            {
-                'AllowedHeaders': ['*'],
-                'AllowedMethods': ['PUT', 'DELETE', 'GET', 'POST', 'HEAD'],
-                'AllowedOrigins': ['*'],
-                'ExposeHeaders': ['PUT', 'DELETE', 'GET', 'POST', 'HEAD'],
-                'MaxAgeSeconds': 3000
-            }
-        ]}
-
-    else:
-        cors_config = cdef.get("CORS")
+    cors_config = get_cors_config_from_cdef(cdef)
 
     try:
         response = s3.put_bucket_cors(Bucket=bucket_name, CORSConfiguration=cors_config)
@@ -539,7 +620,50 @@ def delete_bucket_website():
             eh.retry_error("Bucket Website Error", 90)
             eh.add_log("Bucket Website Error", {"error": str(e)}, is_error=True)
 
-#Not implementing yet
+@ext(handler=eh, op="get_bucket_encryption")
+def get_bucket_encryption(cdef):
+    bucket_name = eh.state.get("bucket_name")
+
+    desired_encryption = {
+        "ServerSideEncryptionConfiguration": {
+            "Rules": [
+                {
+                    "ApplyServerSideEncryptionByDefault": {
+                        "SSEAlgorithm": "aws:kms" if cdef.get("default_kms_key_id") else "AES256",
+                        "KMSMasterKeyID": cdef.get("default_kms_key_id")
+                    },
+                    "BucketKeyEnabled": cdef.get("bucket_key_enabled")
+                }
+            ]
+        }
+    }
+
+    try:
+        encryption = s3.get_bucket_encryption(Bucket=bucket_name)
+        eh.add_log("Got Bucket Encryption", {"encryption": encryption})
+
+        if encryption != desired_encryption:
+            eh.add_op("set_bucket_encryption", desired_encryption)
+
+    except ClientError as e:
+        handle_common_errors(e, eh, "Get Bucket Encryption Failure", 91)
+
+@ext(handler=eh, op="set_bucket_encryption")
+def set_bucket_encryption():
+    bucket_name = eh.state.get("bucket_name")
+    encryption = eh.ops['set_bucket_encryption']
+
+    try:
+        _ = s3.put_bucket_encryption(
+            Bucket=bucket_name,
+            ServerSideEncryptionConfiguration=encryption['ServerSideEncryptionConfiguration']
+        )
+        eh.add_log("Set Bucket Encryption", {"encryption": encryption})
+
+    except ClientError as e:
+        handle_common_errors(e, eh, "Set Bucket Encryption Failure", 92)
+
+
 @ext(handler=eh, op="delete_bucket")
 def delete_bucket():
     bucket_name = eh.state.get("bucket_name")
@@ -595,3 +719,58 @@ def generic_render_def(obj, update_f):
 
 def generate_alternate_bucket_name():
     return f"ck-{random_id()}"
+
+def get_acl_params_from_cdef(cdef):
+    if cdef.get("public_read") == True:
+        params = {
+            "GrantRead": "uri=http://acs.amazonaws.com/groups/global/AllUsers"
+        }
+
+    elif cdef.get("acl"):
+        params = cdef.get("acl") or {}
+
+    else:
+        params = {"ACL": "private"}
+
+    return params
+
+def get_public_access_block_from_cdef(cdef):
+    if (cdef.get("block_public_access") == True or cdef.get("public_access_block") == True):
+        desired_public_access_block = {
+            "BlockPublicAcls": True,
+            "IgnorePublicAcls": True,
+            "BlockPublicPolicy": True,
+            "RestrictPublicBuckets": True
+        }
+    
+    else:
+        desired_public_access_block = cdef.get("public_access_block")
+
+    return desired_public_access_block
+
+def get_cors_config_from_cdef(cdef):
+    if cdef.get("CORS") == True:
+        cors_config = {"CORSRules": [
+            {
+                'AllowedHeaders': ['*'],
+                'AllowedMethods': ['PUT', 'DELETE', 'GET', 'POST', 'HEAD'],
+                'AllowedOrigins': ['*'],
+                'ExposeHeaders': ['PUT', 'DELETE', 'GET', 'POST', 'HEAD'],
+                'MaxAgeSeconds': 3000
+            }
+        ]}
+
+    else:
+        cors_config = cdef.get("CORS")
+    
+    return cors_config
+
+def get_grantee_from_grant_string(grant_string):
+    key, grantee = grant_string.split("=")
+    if key == "id":
+        key = "ID"
+    elif key == "email":
+        key = "EmailAddress"
+    elif key == "uri":
+        key = "URI"
+    return {key: grantee}
